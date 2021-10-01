571811b4e63e4531ef6201525b24cb1a9765a218
18rkuce <91226865+18rkuce@users.noreply.github.com>
2021-09-30 23:41:38 -0500

    Committing Hello World

--- webproject/src/MyServletKucera.java
+++ webproject/src/MyServletKucera.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,14 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyServlet
  */
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
+@WebServlet("/MyServletKucera")
+public class MyServletKucera extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyServlet() {
+    public MyServletKucera() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +26,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Hello World!");
 	}
 
 	/**
